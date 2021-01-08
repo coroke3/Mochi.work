4e37b77d09b20756f1112721f48c249f1171988c
Mochi <59435515+coroke3@users.noreply.github.com>
2021-01-08 20:07:27 +0900

    今日はここまで

---
+++ animation.js
@@ -0,0 +1,10 @@
+$(window).on('load scroll', function() {
+    $(".show").each(function() {
+       var winScroll = $(window).scrollTop();
+       var winHeight = $(window).height();
+       var scrollPos = winScroll + (winHeight * 0.8);
+       if($(this).offset().top < scrollPos) {
+          $(this).css({opacity: 1, transform: 'translate(0, 0)'});
+       }
+    });
+ });
